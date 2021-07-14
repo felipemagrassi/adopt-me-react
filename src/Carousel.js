e442220e065db30f1d1d180904ffe9490c04b966
Felipe Magrassi <felipe.1magrassi@gmail.com>
2021-07-14 10:38:58 -0300

    Added error boundaries, carousel for pic

---
+++ src/Carousel.js
@@ -0,0 +1,43 @@
+import { Component } from "react";
+
+class Carousel extends Component {
+  state = {
+    active: 0,
+  };
+
+  handleIndexClick = (event) => {
+    this.setState({
+      active: +event.target.dataset.index,
+    });
+  };
+
+  static defaultProps = {
+    images: ["http://pets-images.dev-apis.com/pets/none.jpg"],
+  };
+
+  render() {
+    const { active } = this.state;
+    const { images } = this.props;
+
+    return (
+      <div className="carousel">
+        <img src={images[active]} alt="animal" />
+        <div className="carousel-smaller">
+          {images.map((photo, index) => (
+            // eslint-disable-next-line
+            <img
+              key={photo}
+              src={photo}
+              data-index={index}
+              onClick={this.handleIndexClick}
+              alt="animal thumbnail"
+              className={index === active ? "active" : ""}
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Carousel;
